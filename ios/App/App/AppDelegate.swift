bc6c1a8f4fdc0b92ce144062410cc00224028f21
Ahmet Tuncer Yasdali <yasdaliahmet@gmail.com>
2025-09-02 11:38:51 +0000

    Push notification handling and Sentry integration added in AppDelegate and Podfile

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -46,4 +46,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
     }
 
+    override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        NotificationCenter.default.post(name: .capacitorDidRegisterForRemoteNotifications, object: deviceToken)
+    }
+
+    override func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        NotificationCenter.default.post(name: .capacitorDidFailToRegisterForRemoteNotifications, object: error)
+    }
+
 }
